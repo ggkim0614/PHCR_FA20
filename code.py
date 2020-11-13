3eee571950d166f1fd81fb9a2a074b11990124f6
George <37966668+ggkim0614@users.noreply.github.com>
2020-11-13 08:50:01 -0800

    Update changes to code.py

--- code.py
+++ code.py
@@ -1,111 +1,90 @@
-# toggle a blinking led with button push
-
-
-
-# import modules and libraries
-
-import board
-
+# import necessary modules
+from adafruit_circuitplayground import cp
 from digitalio import DigitalInOut, Direction, Pull
-
 import time
+import board
+import neopixel
+import adafruit_hcsr04
+
+# variables assigned for sonar sensor
+sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.A3, echo_pin=board.A2)
+
+# function to find pattern
+def find_array_pattern(data, pattern):
+    count = 0
+    for i in range(len(data)-len(pattern)+1):
+        tmp = [data[i], data [i+1]]
+
+        try:
+            for j in range(len(data)-i):
+                #print(i, i+j)
+                if tmp[-1] != data[i+j+1]:
+                    tmp.append(data[i+j+1])
+
+                if len(tmp) == len(pattern):
+                    #print(tmp)
+                    break
+        except:
+            pass
+
+        if tmp == pattern:
+            count +=1
+
+    return count
 
 
-
-# declare variables and objects
-
-led = DigitalInOut(board.A7)
-
-led.direction = Direction.OUTPUT
-
-ledMode = 0
-
-
-
-button = DigitalInOut(board.A1)
-
-button.direction = Direction.INPUT
-
-button.pull = Pull.DOWN
-
-buttonPre = False
-
-
-
-blinkInterval = 0.5
-
-
-
-# time.monotonic() always INCREASES! from program start
-
-blinkTime = time.monotonic() + blinkInterval
-
-
-
-# repeat this code forever
-
+# loop this forEVER
 while True:
 
-    # gather input
+    # variable declarations for the pushup loop
+    pushup_counter = 0
+    moveRecord = []
 
-    # is the button CHANGED?
+    # actual distance loop
+    while True:
+        print('looping loop')
+        #print(moveRecord)
 
-    if button.value != buttonPre:
+        if find_array_pattern(moveRecord, [1,2,1]) is 1:
+            pushup_counter += 1
+            moveRecord = []
 
-        # save the button value for the next loop
-
-        buttonPre = button.value
-
-
-
-        # is the button pushed?
-
-        if button.value:
-
-            # increment the ledMode
-
-            ledMode += 1
-
-            # constrain the ledMode, lo op to 0
-
-            if ledMode > 1:
-
-                ledMode = 0
+        print('Number of push-ups: ', pushup_counter)
 
 
-
-    # do output according to mode
-
-    # is the ledMode 1?
-
-    if ledMode == 1:
-
-        if time.monotonic() >= blinkTime:
-
-            print("blink")
-
-            # toggle the led value
-
-            led.value = not led.value
-
-            # increment the time by blinkDelay
-
-            blinkTime += blinkInterval
+        # print distance value in cm
+        # in push-up posture, the distance from upper body to the ground is approx. 37~40cm
+        try:
+            sonar_distance = sonar.distance
+        # if in runtime error, make neopixel orange
+        except RuntimeError:
+            print("Runtime Error")
+            continue
 
 
+        # print('the distance is: ', sonar_distance, 'cm')
+        # if distance is bigger than 25cm, make neopixel red
+        if sonar_distance >= 25:
+            cp.pixels[0] = (255, 0, 0)
 
-    else:
+            passing = True
+            print('Going down')
+            #time.sleep(1)
+            moveRecord.append(1)
 
-        # turn led off
+        # if in the process of push-up, blink with yellow
+        elif sonar_distance < 25 and sonar_distance > 10:
+            cp.pixels[0] = (255, 150, 0)
 
-        led.value = False
-
-        # reset the blink time
-
-        blinkTime = time.monotonic()
-
+            print('Not there yet')
+            #time.sleep(1)
 
 
-    # sleep for serial debug
+        # if distance is less than 10cm, make neopixel teal
+        elif sonar_distance <= 10:
+            cp.pixels[0] = (0, 255, 50)
 
-    time.sleep(0.01)
+            print('Go up!')
+            moveRecord.append(2)
+
+    time.sleep(0.25)
